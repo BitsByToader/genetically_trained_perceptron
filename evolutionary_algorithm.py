1b584930f175a608477eefce413a029b0915331e
Tudor Ifrim <ifrimtudor@protonmail.com>
2025-01-03 19:12:00 +0200

    Further modularise and document code.

--- evolutionary_algorithm.py
+++ evolutionary_algorithm.py
@@ -1,30 +1,7 @@
 import random
 import math
 from ioptimization_problem import IOptimizationProblem
-
-
-class Chromosome:
-    def __init__(self, no_genes: int, min_values, max_values):
-        self.no_genes = no_genes
-        self.genes = [0.0] * no_genes
-        self.min_values = list(min_values)
-        self.max_values = list(max_values)
-        self.fitness = 0.0
-        self._initialize_genes()
-
-    def _initialize_genes(self):
-        for i in range(self.no_genes):
-            self.genes[i] = self.min_values[i] + random.random() * (self.max_values[i] - self.min_values[i])
-
-    def __copy__(self):
-        return Chromosome(self.no_genes, self.min_values, self.max_values)
-
-    def copy_from(self, other):
-        self.no_genes = other.no_genes
-        self.genes = list(other.genes)
-        self.min_values = list(other.min_values)
-        self.max_values = list(other.max_values)
-        self.fitness = other.fitness
+from chromosome import Chromosome
 
 
 class Selection:
--- ioptimization_problem.py
+++ ioptimization_problem.py
@@ -1,7 +1,12 @@
+from chromosome import Chromosome
+
+
 # Interface for optimization problems
 class IOptimizationProblem:
-    def compute_fitness(self, chromosome):
+    # Computes the fitness of the given chromosome according to this optimization problem.
+    def compute_fitness(self, chromosome: Chromosome):
         raise NotImplementedError("This method needs to be implemented by a subclass")
 
-    def make_chromosome(self):
+    # Creates a new chromosome specific to this optimization problem.
+    def make_chromosome(self) -> Chromosome:
         raise NotImplementedError("This method needs to be implemented by a subclass")
