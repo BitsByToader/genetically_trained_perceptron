c9cc4d8874c764539227d2675f51a7ead8db3d63
Tudor Ifrim <ifrimtudor@protonmail.com>
2025-01-04 20:46:12 +0200

    Dataset can now be split into training and evaluation vectors.

--- dataset.py
+++ dataset.py
@@ -1,16 +1,23 @@
+from random import shuffle
+
+
 # Class is responsible for loading and storing a training/evaluation dataset for a neural network.
 class Dataset:
     # Constructs an empty dataset
     def __init__(self):
-        self.dataset_path = ""
-        self.input_count = 0
-        self.output_count = 0
+        self.dataset_path: str = ""
+        self.input_count: int = 0
+        self.output_count: int = 0
         
         # Element of list will be a list containing two elements:
         #   1. list of input data;
         #   2. list of corresponding output data.
         # The two lists will only contain floats.
-        self.dataset_vectors = []
+        self.dataset_vectors: [[float]] = []
+
+        # Same as above
+        self.training_vectors: [[float]] = []
+        self.evaluation_vectors: [[float]] = []
 
 
     # Constructs a dataset based on a given file found at dataset_path.
@@ -21,7 +28,7 @@ class Dataset:
 
     # Processes the dataset file found at dataset_path.
     def process_dataset_file(self):
-        dataset_file = open(dataset_path)
+        dataset_file = open(self.dataset_path)
         
         dataset_structure_str = dataset_file.readLine()
         dataset_structure = dataset_structure_str.decode("utf-8").split(',')
@@ -42,3 +49,12 @@ class Dataset:
             self.dataset_vectors.append(dataset_vector)
 
         f.close()
+
+    # Given a rate of evaluation vectors to training vectors, shuffles the dataset and splits it into a list
+    # of training vectors and a list of evaluation vectors.
+    def split_dataset_vectors(self, evaluation_to_training_rate: float):
+        shuffle(self.dataset_vectors)
+        training_vector_count: int = int(evaluation_to_training_rate * float(len(self.dataset_vectors)))
+        
+        self.training_vectors = self.dataset_vectors[0:training_vector_count]
+        self.evaluation_vectors = self.dataset_vectors[training_vector_count:]
