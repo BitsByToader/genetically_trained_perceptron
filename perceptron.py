306f855d62294126a9bd5bc1640c39c1450dda9a
Bulau Bianca <vasilica-bianca.bulau@student.tuiasi.ro>
2025-01-04 18:32:54 +0200

    Further enhancements to Perceptron class

--- perceptron.py
+++ perceptron.py
@@ -1,14 +1,38 @@
-class Perceptron:
-    def __init__(self, input, hidden_layers, nr_output):
-        self.input = input
-        self.hidden_layers = hidden_layers #a list that contains nr of neurons per hidden layer
-        self.nr_output = nr_output
+import math 
 
+class Perceptron:
+    # Constructs an empty perceptron
+    def __init__(self):
+        self.input_count: int = 0
+        self.output_count: int = 0
+        self.hidden_layers_count: int = 0
+        self.neutrons_per_layer: [int] = []
+        self.output_data: [float] = []
+        self.weights: [[[float]]]= []
+        self.theta: [[float]] = []
+
+    # Constructs a perceptron with given information
+    @staticmethod
+    def from_counts(input_count: int, output_count: int, hidden_layers_count: int, neutrons_per_layer: list):
+        perceptron = Perceptron()
+        perceptron.input_count = input_count
+        perceptron.output_count = output_count
+        perceptron.hidden_layers_count = hidden_layers_count
+        perceptron.neutrons_per_layer = neutrons_per_layer
+        
+        perceptron.weight_initialization()
+        perceptron.theta_initialization()
+        return perceptron
+    
+    @staticmethod
+    def from_file(weights: [[[float]]]):
+        perceptron = Perceptron()
+        perceptron.weights = weights
+
+    # Initialize weights according to the available data
     def weight_initialization(self):
-        layers = [len(self.input)] + self.hidden_layers + [self.nr_output]
-        print(layers)
-        self.weights = []
-        self.theta = []
+        layers = [self.input_count] + self.neutrons_per_layer + [self.output_count]
+        print("nr neurons per layer: ",layers)
 
         for i in range(1,len(layers)):
             a = []
@@ -16,35 +40,51 @@ class Perceptron:
                 b = [0.0] * layers[i - 1]
                 a.append(b)
             self.weights.append(a)
-        
+    
+    # Initialize theta according to the available data
+    def theta_initialization(self):
+        layers = [self.input_count] + self.neutrons_per_layer + [self.output_count]
+
         for i in range(1,len(layers)):
-            b = [-1.0] * layers[i]
+            b = [0.0] * layers[i]
             self.theta.append(b)
 
-    def compute_output(self):
-        x  = self.input
+    # Given an input the perceptron calculates the output
+    def compute_output(self, input: list):
+        actual_output = input
+        
         for layer in range(len(self.weights)):
             inner_input = [1.0] * len(self.weights[layer])
             for i in range(len(self.weights[layer])):    
                 for j in range(len(self.weights[layer][i])):
-                    inner_input[i] *= x[j] * self.weights[layer][i][j]
+                    inner_input[i] *= actual_output[j] * self.weights[layer][i][j]
+                
                 inner_input[i] -= self.theta[layer][i]
-            x = inner_input
-            print(x)
-        self.actual_output = x
+                inner_input[i] = self.sigmoid_activation(inner_input[i]) # Activation function
+            
+            actual_output = inner_input
 
-    def compute_error(self, desired_output):
-        err = [0.0] * len(self.actual_output)   
+        self.output_data = actual_output
+        print("Actual output: ",self.output_data)
+
+    # Bipolar sigmoid activation function
+    @staticmethod
+    def sigmoid_activation(x: float):
+        return (1 - math.exp(-2*x))/(1 + math.exp(-2*x))
+    
+    # Calculates the mean square error
+    def compute_error(self, desired_output: list):
+        err = [0.0] * self.output_count 
         mean_error = 0.0
-        for i in range(len(self.actual_output)):
-            err[i] = (1/2) * (self.actual_output[i] - desired_output[i]) * (self.actual_output[i] - desired_output[i])
-            mean_error+=err[i]
+        for i in range(self.output_count):
+            err[i] = (desired_output[i] - self.output_data[i]) * (desired_output[i] - self.output_data[i])
+            mean_error += err[i]
         mean_error /= len(err)
+        print("Mean error: ",mean_error)
     
 if __name__ == '__main__':
-    perceptron = Perceptron([1, 1, 1, 1, 1], [3, 2, 3], 4)
-    perceptron.weight_initialization()
-    print(perceptron.weights)
-    print(perceptron.theta)
-    perceptron.compute_output()
+    perceptron = Perceptron.from_counts(5, 4, 3, [3, 2, 3])
+    print("weights: ",perceptron.weights)
+    print("theta: ",perceptron.theta)
+    perceptron.compute_output([1, 1, 1, 1, 1])
     perceptron.compute_error([0, 0, 0, 0])
